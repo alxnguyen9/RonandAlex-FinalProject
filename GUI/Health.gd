9a5ab2b6ff1822812f212043f10e4cec001446ef
Alex Nguyen <anguyen@Alexs-MacBook-Pro.local>
2025-10-06 15:18:09 -0400

    Sprint 1 release

--- GUI/Health.gd
+++
@@ -1,9 +0,0 @@
-### Health.gd
-extends ColorRect
-
-#ref to our label node
-@onready var label = $Label
-
-# updates label text when signal is emitted
-func update_lives(lives, max_lives):
-	label.text = str(lives)
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -7,32 +7,6 @@ var is_attacking = false
 var is_climbing = false
 var is_jumping = false
 
-var can_spawn = true
-
-#current scene
-var current_scene_name
-
-#bomb movement state
-var is_bomb_moving = false
-
-#can the player be damaged?
-var can_hurt = true
-
-#pickups
-enum Pickups {HEALTH, SCORE, ATTACK}
-
-func _ready():
-	# Sets the current scene's name
-	current_scene_name = get_tree().get_current_scene().name
-
-#function to disable box spawning
-func disable_spawning():
-	can_spawn = false
-	
-#function to enable box spawning
-func enable_spawning():
-	can_spawn = true
-
 #sets is_climbing to true to simulate climbing
 func _on_body_entered(body):
 	if body.name == "Player":
